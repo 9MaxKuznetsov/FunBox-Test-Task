411a4a7a176899943ed08e64171f5c94fb9d35ca
AlexanderBrez <jztenk@gmail.com>
2018-10-18 17:59:51 +0300

    Mouseout troubles

--- source/js/actions.js
+++ source/js/actions.js
@@ -1,9 +1,35 @@
 var item = document.querySelector(".option-wrapper");
 
-var itemtoggle = function () {
-  item.style.background = "linear-gradient(135deg, transparent 31px, black 0) top left";
-}
+var itemclick = function () {
+  item.classList.toggle("option-wrapper--selected");
+};
 
-item.addEventListener('onmouseout', function () {
-    itemtoggle();
+var itemselectedhover = function () {
+    item.classList.toggle("option-wrapper--selected-hover");
+};
+
+var itemhover = function () {
+  item.classList.toggle("option-wrapper-hover");
+};
+
+item.addEventListener('click', function () {
+    itemclick();
 });
+
+item.addEventListener('mouseout', function() {
+  if (item.classList.contains("option-wrapper--selected")) {
+    itemselectedhover();
+    console.log(123);
+  } else itemhover();
+});
+
+/*
+
+for (var i = 0; i < item.length; i++) {
+  item[i].addEventListener("click", function(event) {
+    event.preventDefault();
+    item.classList.toggle("option-wrapper--selected");
+  });
+};
+
+*/
