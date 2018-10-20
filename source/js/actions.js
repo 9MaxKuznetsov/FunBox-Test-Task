debed3e8bbc3104c3deb7ad261f68e34c0fb8e80
9MaxKuznetsov <jztenk@gmail.com>
2018-10-20 23:32:20 +0300

    some actions

--- source/js/actions.js
+++ source/js/actions.js
@@ -1,35 +1,8 @@
-var item = document.querySelector(".option-wrapper");
+const items = document.querySelectorAll('.option-wrapper');
 
-var itemclick = function () {
-  item.classList.toggle("option-wrapper--selected");
-};
 
-var itemselectedhover = function () {
-    item.classList.toggle("option-wrapper--selected-hover");
-};
-
-var itemhover = function () {
-  item.classList.toggle("option-wrapper-hover");
-};
-
-item.addEventListener('click', function () {
-    itemclick();
-});
-
-item.addEventListener('mouseout', function() {
-  if (item.classList.contains("option-wrapper--selected")) {
-    itemselectedhover();
-    console.log(123);
-  } else itemhover();
-});
-
-/*
-
-for (var i = 0; i < item.length; i++) {
-  item[i].addEventListener("click", function(event) {
-    event.preventDefault();
-    item.classList.toggle("option-wrapper--selected");
+[].forEach.call(items, (item) => {
+  item.addEventListener('click', () => {
+    item.classList.toggle('option-wrapper--selected');
   });
-};
-
-*/
+})
