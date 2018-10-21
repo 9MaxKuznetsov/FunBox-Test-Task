65a1c4d73f8e3fda9a9d24fa9753e90e7b93b175
AlexanderBrez <jztenk@gmail.com>
2018-10-21 19:25:11 +0300

    Safari wide borders fix

--- source/js/actions.js
+++ source/js/actions.js
@@ -1,8 +1,9 @@
 const items = document.querySelectorAll('.option-wrapper');
+const buttons = document.querySelectorAll('.buy__button');
 
 
 [].forEach.call(items, (item) => {
   item.addEventListener('click', () => {
     item.classList.toggle('option-wrapper--selected');
   });
-})
+});
